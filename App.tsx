e70351b07bcdbd6271bf0b0c45629e89d35cffe5
Daniel Faustino <danieloliveirafaustino@gmail.com>
2022-09-12 19:06:31 -0300

    initial files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+interface ButtonProps {
+  title: string
+}
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text> {props.title}</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text >Simbora!</Text>
+
+      <Button title="Nlw eSports" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#000',
+    fontSize: 22
+  },
+  button: {
+    padding: '2%',
+    backgroundColor:'#666',
+    borderRadius:5
+  }
 });
