d8e41a164887efea20049a17e3fa2b3de1003018
Daniel Faustino <dfaustino@fastprobr.com>
2022-09-15 13:19:12 -0300

    feat: new components

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,36 +1,35 @@
+import { LinearGradient } from "expo-linear-gradient";
 import {
+  ImageBackground,
+  Text,
   TouchableOpacity,
   TouchableOpacityProps,
-  ImageBackground,
-  ImageSourcePropType,
-  Text
-} from 'react-native'
+} from "react-native";
+import { THEME } from "../../theme";
+import { styles } from "./styles";
 
-import { styles } from './styles'
-
-import { LinearGradient } from 'expo-linear-gradient'
-import { THEME } from '../../theme'
-
-export interface GameCardProps {
-  id: string
-  name: string
-  ads: string
-  cover: ImageSourcePropType
+export interface GameCardsProps {
+  id: string;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
-  data: GameCardProps
+  data: GameCardsProps;
 }
 
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground style={styles.cover} source={data.cover}>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{data._count.ads} Ads</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
-  )
+  );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,52 @@
-import { View, Image, FlatList } from 'react-native'
+import { useEffect, useState } from "react";
+import { FlatList, Image } from "react-native";
+import { styles } from "./styles";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
 
-import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard'
-import { Heading } from '../../components/Heading'
+import { Heading } from "../../components/Heading";
+import { GameCard, GameCardsProps } from "../../components/GameCard";
 
-import { GAMES } from '../../utils/games'
-import { styles } from './styles'
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Background } from "../../components/Background";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardsProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.0.158:5556/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardsProps) {
+    navigation.navigate("game", { id, title, bannerUrl });
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={logoImg} style={styles.logo} />
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo} />
 
-      <Heading
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..."
-      />
-
-      <FlatList
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-    </View>
-  )
+        <Heading
+          title="Find your duo!"
+          subtitle="Choose the game you'd like to play..."
+        />
+        <FlatList
+          contentContainerStyle={styles.contentList}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={games}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <GameCard data={item} onPress={() => handleOpenGame(item)} />
+          )}
+        />
+      </SafeAreaView>
+    </Background>
+  );
 }
